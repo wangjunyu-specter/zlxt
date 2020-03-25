618159c62c2c8d76c4a6ff80909a635dc78de475
wjy <348805651@qq.com>
2020-03-26 01:26:09 +0800

    'a'

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,7 +2,7 @@
  * @Author: wjy-mac
  * @Date: 2020-03-24 15:44:45
  * @LastEditors: wjy-mac
- * @LastEditTime: 2020-03-24 23:39:29
+ * @LastEditTime: 2020-03-26 01:11:15
  * @Description: file content
  */
 import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
@@ -20,7 +20,7 @@ export class AboutComponent implements OnInit, AfterViewInit  {
   type: number; // 1 公司介绍 2成达介绍
   swiperdata: any; // swiper 实例 
   activeIndex: number; // 当前swiper序号
-  constructor(private route: ActivatedRoute, private elementRef: ElementRef) { }
+  constructor(private route: ActivatedRoute, private elementRef: ElementRef, private router: Router) { }
 
   ngOnInit(): void {
     this.type = 1;
@@ -50,5 +50,8 @@ export class AboutComponent implements OnInit, AfterViewInit  {
   }
   moveEnd() {
     console.log('结束')
+  }
+  setBack() {
+    this.router.navigate(['/home'])
   }
 }
--- src/app/aboutcd/aboutcd.component.ts
+++ src/app/aboutcd/aboutcd.component.ts
@@ -1,3 +1,11 @@
+import { ActivatedRoute, Router } from '@angular/router';
+/*
+ * @Author: wjy-mac
+ * @Date: 2020-03-24 23:37:32
+ * @LastEditors: wjy-mac
+ * @LastEditTime: 2020-03-26 01:09:54
+ * @Description: file content
+ */
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +15,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutcdComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:Router) { }
 
   ngOnInit(): void {
   }
-
+  setBack() {
+    this.route.navigate(['/home'])
+  }
 }
